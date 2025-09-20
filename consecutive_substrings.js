74ebe4cfaa8eb6e8661a53fa526afe9c596dd43e
Gerry <gerrywiegand@gmail.com>
2025-09-20 14:30:42 -0400

    done

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,18 +1,27 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  if (string.length === 0) return [];
+
+  const subStrings = [];
+
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + 1; j <= string.length; j++) {
+      subStrings.push(string.slice(i, j));
+    }
+  }
+  return subStrings;
 }
+
+console.log(consecutiveSubstrings("abc"));
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
-  console.log("=>", consecutiveSubstrings('abc'));
+  console.log("=>", consecutiveSubstrings("abc"));
 
   console.log("");
 
   console.log("Expecting: ['a']");
-  console.log("=>", consecutiveSubstrings('a'));
+  console.log("=>", consecutiveSubstrings("a"));
 }
 
 module.exports = consecutiveSubstrings;
-
-
